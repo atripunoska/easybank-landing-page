28e2b2487d7f5a03775165a0fb357ca2a3a10e2a
atripunoska <atripunoska@gmail.com>
2024-03-15 14:26:26 +0100

    feat: customize tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "hsl(233, 26%, 24%)",
+        limeGreen: "hsl(136, 65%, 51%)",
+        brightCyan: "hsl(192, 70%, 51%)",
+        grayishBlue: "hsl(233, 8%, 62%)",
+        lightGrayishBlue: "hsl(220, 16%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
